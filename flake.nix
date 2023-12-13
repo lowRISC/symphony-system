4bd2ad148732d20e074f49e16f64a1698c4de67e
Hugo McNally <hugo.mcnally@gmail.com>
2023-12-13 15:45:55 +0000

    Added a python check and fix apps to nix.
    Signed-off-by: Hugo McNally <hugo.mcnally@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Symphony System";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: let
+    system_outputs = system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      python_check_app = pkgs.writeShellApplication {
+        name = "python-check";
+        runtimeInputs = with pkgs; [
+          ruff
+          python311
+          python311Packages.mypy
+        ];
+        text = ''
+          ruff format --check .
+          ruff check .
+          mypy .
+        '';
+      };
+      python_fix_app = pkgs.writeShellApplication {
+        name = "python-fix";
+        runtimeInputs = with pkgs; [ ruff ];
+        text = ''
+          ruff format .
+          ruff check --fix .
+        '';
+      };
+    in {
+      formatter = pkgs.alejandra;
+      apps.python_check = {
+        type = "app";
+        program = "${python_check_app}/bin/python-check";
+      };
+      apps.python_fix = {
+        type = "app";
+        program = "${python_fix_app}/bin/python-fix";
+      };
+    };
+  in
+    flake-utils.lib.eachDefaultSystem system_outputs;
+}
